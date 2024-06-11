da86c56613ac1b05bec3de712594ab986f193bc8
Andrew Lytle <atlytle@gmail.com>
2024-06-11 12:40:01 -0500

    Wrap C++ weight object

--- use_ClebschGordan.py
+++ use_ClebschGordan.py
@@ -7,7 +7,6 @@ from itertools import product
 
 import numpy as np
 
-#from sympy import N
 from sympy.physics.quantum.cg import CG, Wigner3j, Wigner6j
 
 import ClebschGordan
@@ -15,7 +14,6 @@ import ClebschGordan
 
 def test_binomial():
     binomial = ClebschGordan.binomial_t()
-
     print(f"{binomial(5,2) = }")
 
 
@@ -44,9 +42,30 @@ def compare_sympy():
                 cg = CG(j1, m1, j2, m2, j3, m3).doit()
                 if cg != 0:
                     print(f"{m1 = }, {m2 = }, {m3 = }")
-                    print(f"{cg = }")  
+                    print(f"{cg = }")
 
-def calcCGs(N, _S, _Sprime, _Sdoubleprime):
+class Weight:
+    """Wrapper for ClebschGordan.weight C++ object.
+    
+    Args:
+        iweight (list): N integers to specify SU(N) irrep weight
+            e.g. [1 0 0] gives the fundamental of SU(3)
+    """
+    def __init__(self, iweight):
+        self.N = len(iweight)  # Lie group SU(N).
+        self.weight = ClebschGordan.weight(self.N)  # C++ weight object.
+        # Assign weight element-wise. (Workaround for C++/swig)
+        for i, x in enumerate(iweight):
+            self.weight[i+1] = x
+        self.dim = self.weight.dimension()
+
+    def init_weight(S, _S):
+        "Assign weight element-wise. (Workaround for C++/swig)"
+        for i, x in enumerate(_S):
+            S[i+1] = x
+
+
+def calcCGs(_S, _Sprime, _Sdoubleprime):
     """Calculate Clebsch-Gordan coefficients for S x S' -> S''"
 
     Args:
@@ -54,72 +73,41 @@ def calcCGs(N, _S, _Sprime, _Sdoubleprime):
         _S (list): N integers to specify the irrep weight e.g. [2 1 0].
     """
 
-    def init_weight(S, _S):
-        "Assign weight element-wise. (Workaround for C++/swig)"
-        for i, x in enumerate(_S):
-            S[i+1] = x
+    S = Weight(_S)
+    Sprime = Weight(_Sprime)
+    Sdoubleprime = Weight(_Sdoubleprime)
 
-    S = ClebschGordan.weight(N)
-    Sprime = ClebschGordan.weight(N)
-    Sdoubleprime = ClebschGordan.weight(N)
-
-    #print('weight.dimension =', weight.dimension())
-
-    init_weight(S, _S)
-    init_weight(Sprime, _Sprime)
-    init_weight(Sdoubleprime, _Sdoubleprime)
-    dimS = S.dimension()
-    dimSprime = Sprime.dimension()
-    dimSdoubleprime = Sdoubleprime.dimension()
-
-    coeffs = ClebschGordan.coefficients(Sdoubleprime, S, Sprime)
+    coeffs = ClebschGordan.coefficients(Sdoubleprime.weight, 
+                                        S.weight, Sprime.weight)
     print("Q' Q Q''")
-    for i in range(dimSdoubleprime):
-        for j in range(dimSprime):
-            for k in range(dimS):
+    for i in range(Sdoubleprime.dim):
+        for j in range(Sprime.dim):
+            for k in range(S.dim):
                 c = coeffs(j, k, 0, i)
                 if abs(c) > 0.00001:
                     print(f"{j+1} {k+1} {i +1} {coeffs(j, k, 0, i)}")
 
-def CG_swig(N, w1, m1, w2, m2, w3, m3):
+def CG_swig(w1, m1, w2, m2, w3, m3):
     """CG coefficient appearing in w1 x w2-> w3.
 
     Args:
-        N (int): Specify Lie group SU(N)
         w1,... (list): N integers to specify irrep weight e.g. [2 1 0]
         m1,... (int): Quantum number associated to w1
     """
-
-    def init_weight(S, _S):
-        "Assign weight element-wise. (Workaround for C++/swig)"
-        for i, x in enumerate(_S):
-            S[i+1] = x
-
-    S = ClebschGordan.weight(N)
-    Sprime = ClebschGordan.weight(N)
-    Sdoubleprime = ClebschGordan.weight(N)
-
-    #print('weight.dimension =', weight.dimension())
-
-    init_weight(S, w1)
-    init_weight(Sprime, w2)
-    init_weight(Sdoubleprime, w3)
-    # dimS = S.dimension()
-    # dimSprime = Sprime.dimension()
-    # dimSdoubleprime = Sdoubleprime.dimension()
+    S = Weight(w1)
+    Sprime = Weight(w2)
+    Sdoubleprime = Weight(w3)
 
     # Calculates all coefficients.
-    coeffs = ClebschGordan.coefficients(Sdoubleprime, S, Sprime)
+    coeffs = ClebschGordan.coefficients(Sdoubleprime.weight, 
+                                        S.weight, Sprime.weight)
 
     return coeffs(m1, m2, 0, m3)  # Set multiplicity index to 0 for now..
 
 def test_CG_swig(w1, w2, w3):
     """Compare output of CG_swig with sympy.CG (SU(2) only).
     """
-    N = 2
-    #w1 = [1, 0]
-    #w2 = [1, 0]
-    #w3 = [2, 0]
+    assert(len(w1) == 2)  # Check that N=2.
     j1 = w1[0]/2
     j2 = w2[0]/2
     j3 = w3[0]/2
@@ -133,7 +121,7 @@ def test_CG_swig(w1, w2, w3):
         m3 = -j3 + a3
         #print(f"{m1 = } {m2 = } {m3 = }")
         cg = CG(j1, m1, j2, m2, j3, m3).doit()
-        cg2 = CG_swig(N, w1, a1, w2, a2, w3, a3)
+        cg2 = CG_swig(w1, a1, w2, a2, w3, a3)
         #print(cg)
         #print(cg2)
         _result = math.isclose(cg, cg2)
@@ -142,71 +130,50 @@ def test_CG_swig(w1, w2, w3):
     return np.array(result).all()
 
 def compute_9R(A, B, C, D, E):
-    "Implement Eq. 9 of [2101.10227]."
+    """Implement Eq. 9 of [2101.10227].
 
-    def init_weight(S, _S):
-        "Assign weight element-wise. (Workaround for C++/swig)"
-        for i, x in enumerate(_S):
-            S[i+1] = x
+    Args:
+        A,B,... (list): SU(3) iweight e.g. [2, 1 0]
+    """
 
-    result = 0
-    N = 3  # SU(3).
     three = [1, 0, 0]  # Weight associated to the fundamental(3) of SU(3)
-    _three = ClebschGordan.weight(N) # Fix, this can be 3bar too..
-    init_weight(_three, three)
-    dthree = _three.dimension()
-    # Need to fix this sort of pattern..
-    _A = ClebschGordan.weight(N)
-    init_weight(_A, A)
-    dA = _A.dimension()
-    _B = ClebschGordan.weight(N)
-    init_weight(_B, B)
-    dB = _B.dimension()
-    _C = ClebschGordan.weight(N)
-    init_weight(_C, C)
-    dC = _C.dimension()
-    _D = ClebschGordan.weight(N)
-    init_weight(_D, D)
-    dD = _D.dimension()
-    _E = ClebschGordan.weight(N)
-    init_weight(_E, E)
-    dE = _E.dimension()
+    dthree = Weight(three).dim
+    dA = Weight(A).dim
+    dB = Weight(B).dim
+    dC = Weight(C).dim
+    dD = Weight(D).dim
+    dE = Weight(E).dim
     print(f"{dthree = } {dA = } {dB = } {dC = } {dD = } {dE = } ")
     result = 0
     for a, b, c, d, e, m in product(range(dA), range(dB), range(dC),
                                     range(dD), range(dE), range(dthree)):
         print(a,b,c,d,e,m)
-        result += CG_swig(N, D, d, B, b, E, e)*CG_swig(N, A, a, B, b, C, c)*\
-                  CG_swig(N, A, a, three, m, D, d)*CG_swig(N, C, c, three, m, E, e)
-        print(CG_swig(N, D, d, B, b, E, e))
-        print(CG_swig(N, D, d, B, b, E, e))
-        print(CG_swig(N, A, a, three, m, D, d))
-        print(CG_swig(N, C, c, three, m, E, e))
+        result += CG_swig(D, d, B, b, E, e)*CG_swig(A, a, B, b, C, c)*\
+                  CG_swig(A, a, three, m, D, d)*CG_swig(C, c, three, m, E, e)
+        print(CG_swig(D, d, B, b, E, e))
+        print(CG_swig(D, d, B, b, E, e))
+        print(CG_swig(A, a, three, m, D, d))
+        print(CG_swig(C, c, three, m, E, e))
     return result
 
-def debug_CG():
-    N = 3
-    print(CG_swig(N, [0, 0, 0], 0, [1, 0, 0], 1, [1, 0, 0], 0))
-
-if __name__ == "__main__":
-    #calcCGs(2, [1, 0], [1, 0], [2, 0])
-    
-    #calcCGs(3, [2, 1, 0], [2, 1, 0], [2, 1, 0])
-    #print()
-    #print(CG_swig(3, [2, 1, 0], 2, [2, 1, 0], 0, [2, 1, 0], 0))
-    #print(CG_swig(3, [2, 1, 0], 0, [2, 1, 0], 2, [2, 1, 0], 0))
-    
-    print(test_CG_swig([1, 0], [2, 0], [3, 0]))
-    print(test_CG_swig([2, 0], [3, 0], [3, 0]))
-    print(test_CG_swig([2, 0], [3, 0], [5, 0]))
-
-    '''
+def test_9R():
     A = [1, 0, 0]
     B = [1, 1, 0]
     C = [0, 0, 0]
     D = [1, 1, 0]
     E = [1, 0, 0]
-    print(compute_9R(A, B, C, D, E))
-    print(math.sqrt(3))
-    #debug_CG()
-    '''
+    print(np.isclose(compute_9R(A, B, C, D, E), math.sqrt(3)))
+
+if __name__ == "__main__":
+    #calcCGs([1, 0], [1, 0], [2, 0])
+    
+    #calcCGs([2, 1, 0], [2, 1, 0], [2, 1, 0])
+    #print()
+    #print(CG_swig([2, 1, 0], 2, [2, 1, 0], 0, [2, 1, 0], 0))
+    #print(CG_swig([2, 1, 0], 0, [2, 1, 0], 2, [2, 1, 0], 0))
+    
+    #print(test_CG_swig([1, 0], [2, 0], [3, 0]))
+    #print(test_CG_swig([2, 0], [3, 0], [3, 0]))
+    #print(test_CG_swig([2, 0], [3, 0], [5, 0]))
+
+    test_9R()
